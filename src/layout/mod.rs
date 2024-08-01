9377a0d70219337a58545a55011d88303622e70f
tison <wander4096@gmail.com>
2024-08-01 11:49:46 +0800

    refactor: opt out colored
    Signed-off-by: tison <wander4096@gmail.com>

--- src/layout/mod.rs
+++ src/layout/mod.rs
@@ -12,10 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#[cfg(feature = "colored")]
 pub use colored_simple_text::ColoredSimpleTextLayout;
 use log::Record;
 pub use simple_text::SimpleTextLayout;
 
+#[cfg(feature = "colored")]
 mod colored_simple_text;
 mod kv_display;
 mod simple_text;
@@ -27,6 +29,7 @@ pub trait Layout {
 #[derive(Debug)]
 pub enum LayoutImpl {
     SimpleText(SimpleTextLayout),
+    #[cfg(feature = "colored")]
     ColoredSimpleText(ColoredSimpleTextLayout),
 }
 
