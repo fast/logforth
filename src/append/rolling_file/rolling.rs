f66c2a38129bc50fc070449c2d470651442686e0
tison <wander4096@gmail.com>
2024-08-12 09:42:00 +0800

    use better impl
    Signed-off-by: tison <wander4096@gmail.com>

--- src/append/rolling_file/rolling.rs
+++ src/append/rolling_file/rolling.rs
@@ -277,11 +277,7 @@ impl State {
 
                 if self.log_filename_prefix.is_none()
                     && self.log_filename_suffix.is_none()
-                    && Zoned::strptime(
-                        format!("{} %:z", self.date_format),
-                        format!("{filename} +00:00"),
-                    )
-                    .is_err()
+                    && jiff::civil::DateTime::strptime(self.date_format, filename).is_err()
                 {
                     return None;
                 }
