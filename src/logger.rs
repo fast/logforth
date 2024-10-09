3007fdec2c30070b171c5645c01b8f6b42a8efae
tison <wander4096@gmail.com>
2024-10-09 09:04:14 -0600

    feat: support setting global max_level
    Signed-off-by: tison <wander4096@gmail.com>

--- src/logger.rs
+++ src/logger.rs
@@ -134,6 +134,7 @@ impl Dispatch {
 #[derive(Debug)]
 pub struct Logger {
     dispatches: Vec<Dispatch>,
+    max_level: LevelFilter,
 }
 
 impl Default for Logger {
@@ -146,14 +147,24 @@ impl Logger {
     /// Create a new [`Logger`] instance.
     #[must_use = "call `dispatch` to add a dispatch to the logger and `apply` to set the global logger"]
     pub fn new() -> Logger {
-        Self { dispatches: vec![] }
+        Self {
+            dispatches: vec![],
+            max_level: LevelFilter::Trace,
+        }
     }
-}
 
-impl Logger {
+    /// Set the global maximum log level.
+    ///
+    /// This will be passed to [`log::set_max_level`] on [`Logger::apply`].
+    #[must_use = "call `apply` to set the global logger"]
+    pub fn max_level(mut self, max_level: LevelFilter) -> Self {
+        self.max_level = max_level;
+        self
+    }
+
     /// Add a [`Dispatch`] to the [`Logger`].
     #[must_use = "call `apply` to set the global logger"]
-    pub fn dispatch(mut self, dispatch: Dispatch) -> Logger {
+    pub fn dispatch(mut self, dispatch: Dispatch) -> Self {
         self.dispatches.push(dispatch);
         self
     }
@@ -164,8 +175,9 @@ impl Logger {
     ///
     /// An error is returned if the global logger has already been set.
     pub fn apply(self) -> Result<(), log::SetLoggerError> {
+        let max_level = self.max_level;
         log::set_boxed_logger(Box::new(self))?;
-        log::set_max_level(LevelFilter::Trace);
+        log::set_max_level(max_level);
         Ok(())
     }
 }
@@ -192,7 +204,6 @@ impl log::Log for Logger {
     }
 }
 
-// TODO(tisonkun): logback and log4j2 support custom error handling (status listener).
 fn handle_error(record: &Record, error: anyhow::Error) {
     let Err(fallback_error) = write!(
         std::io::stderr(),
