76cc4c8a8af6995ab99fc0c2af23d29a0d47278b
tison <wander4096@gmail.com>
2025-01-10 14:38:51 +0800

    add docs
    Signed-off-by: tison <wander4096@gmail.com>

--- src/diagnostic/mod.rs
+++ src/diagnostic/mod.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! Markers to enrich log records with additional information.
+//! Mapped Diagnostic Context (MDC).
 
 use std::borrow::Cow;
 
