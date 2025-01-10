941449ef6c0ec9c2eaf3540a1dc0018b126489db
Ashley Mannix <kodraus@hey.com>
2025-01-10 11:04:08 +1000

    fix: a few tests requiring features (#91)

--- tests/recursive_logging.rs
+++ tests/recursive_logging.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#![cfg(feature = "rolling-file")]
+
 use logforth::append;
 use logforth::append::rolling_file;
 use logforth::append::rolling_file::RollingFileWriter;
