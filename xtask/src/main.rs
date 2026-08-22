fe4b4ffe7fd4c2e702a811ec17d22e0bfc0a1042
tison <wander4096@gmail.com>
2026-08-22 19:09:03 +0800

    chore: install latest HawkEye

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -165,7 +165,7 @@ fn make_clippy_cmd(fix: bool) -> StdCommand {
 }
 
 fn make_hawkeye_cmd(fix: bool) -> StdCommand {
-    ensure_installed("hawkeye", "hawkeye@7.0.0");
+    ensure_installed("hawkeye", "hawkeye");
     let mut cmd = find_command("hawkeye");
     if fix {
         cmd.args(["format"]);
